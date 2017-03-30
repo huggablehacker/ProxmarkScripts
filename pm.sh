a87d8ed5f0c1854d99f59cd2ae66be3d742d9f6e
Huggable Hacker <huggablehacker@users.noreply.github.com>
2017-03-30 10:55:13 -0400

    Added OSX KickOff Script

---
+++ pm.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#OSX Version of Proxmark3 Kickoff Script
+#Using Version 2.2 Proxmark Software
+
+cd proxmark3/client/
+./proxmark3 /dev/cu.usbmodem1421 #your usb modem designation may be different#
