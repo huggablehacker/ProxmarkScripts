d57664374509e2295511cafdbf3d3462172817fd
Huggable Hacker <huggablehacker@users.noreply.github.com>
2017-05-04 17:00:37 -0400

    Create linux.sh

---
+++ linux.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Linux Version of the Proxmark3 Kickoff script#
+#Using Version 2.2 of Proxmark Software#
+
+cd proxmark3/Ubuntu_x64/client #proxmark folder sits in my home directory#
+sudo ./proxmark3 /dev/ttyACM0 #your usb modem designation may be different#
